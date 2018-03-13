160b32ed2bff76574e8ea9043da787d33e95b273
BackStreetSoy <jordanedwardobey@gmail.com>
2018-03-13 18:28:23 -0400

    added migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180312194322) do
+ActiveRecord::Schema.define(version: 20180313222445) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 20180312194322) do
     t.string "username"
     t.string "password"
     t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "videos", force: :cascade do |t|
+    t.string "title"
+    t.string "src"
+    t.string "summary"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
